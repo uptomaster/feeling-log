88ea7590c7036570964f3c74c00aec113f418ac7
uptomaster <lnh929@naver.com>
2026-04-29 07:11:27 +0900

    [Feat] 일기 관리 기능 구현하기 1

--- my-diary/src/App.jsx
+++ my-diary/src/App.jsx
@@ -1,57 +1,44 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import New from "./pages/New";
+import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 import NotFound from "./pages/NotFound";
 import { getEmotionImage } from "./util/get-emotion-image";
 import Button from "./components/Button";
 import Header from "./components/Header";
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
 
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  };
+  const [data, dispatch] = useReducer(reducer, mockData);
 
   return (
     <>
-      <Header title={"Header"} 
-      leftChild={<Button text={"Left"} />}
-      rightChild={<Button text={"Right"} />}
-      />
-
-      <Button
-        text={"123"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
-      <Button
-        text={"123"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
-      <Button
-        text={"123"}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
-        {/* 와일드카드 => 위에 세개가 다 아니라면 NotFound로 렌더링 */}
+        <Route path="edit/:id" element={Edit} />
+        {/* 와일드카드 => 위에가 다 아니라면 NotFound로 렌더링 */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
