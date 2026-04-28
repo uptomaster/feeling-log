f5d5b51b38cfa3a230a21308d86ae6bfda9b60b3
uptomaster <lnh929@naver.com>
2026-04-29 06:57:48 +0900

    [Feat] 폰트,이미지,레이아웃 설정 & 공통 컴포넌트 구현

--- my-diary/src/App.jsx
+++ my-diary/src/App.jsx
@@ -1,38 +1,60 @@
 import "./App.css";
-import {Routes, Route, Link, useNavigate} from "react-router-dom"
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import NotFound from "./pages/NotFound";
+import { getEmotionImage } from "./util/get-emotion-image";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 
 function App() {
-
   const nav = useNavigate();
 
   const onClickButton = () => {
     nav("/new");
-  }
+  };
 
   return (
-  <>
-    <div>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/new"}>New</Link>
-      <Link to={"/diary"}>Diary</Link>
-    </div>
-    <button onClick={onClickButton}>New 페이지로 이동</button>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary/:id" element={<Diary />} />
-      {/* 와일드카드 => 위에 세개가 다 아니라면 NotFound로 렌더링 */}
-      <Route path="*" element={<NotFound/>} /> 
-    </Routes>
-  </>
+    <>
+      <Header title={"Header"} 
+      leftChild={<Button text={"Left"} />}
+      rightChild={<Button text={"Right"} />}
+      />
+
+      <Button
+        text={"123"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        {/* 와일드카드 => 위에 세개가 다 아니라면 NotFound로 렌더링 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
