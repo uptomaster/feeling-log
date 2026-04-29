8e54738689c961caba806d74de33ca3415fb21b9
uptomaster <lnh929@naver.com>
2026-04-29 09:53:18 +0900

    [Feat] Home 페이지 구현하기2

--- my-diary/src/components/DiaryList.jsx
+++ my-diary/src/components/DiaryList.jsx
@@ -1,16 +1,37 @@
 import Button from "./Button";
 import './DiaryList.css';
 import DiaryItem from "./DiaryItem";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const DiaryList = ({data}) => {
+
+  const nav = useNavigate();
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e)=>{
+    setSortType(e.target.value);
+  };
+
+  const getSortedDate = ()=>{
+    return data.toSorted((a,b)=>{
+      if(sortType==='oldest'){
+        return a.createdDate - b.createdDate;
+      }
+      else{
+        return b.createdDate - a.createdDate;
+      }
+    });
+  }
+
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select name="" id="">
+        <select onChange={onChangeSortType} name="" id="">
           <option value={"latest"}>최신순</option>
           <option value={"oldest"}>오래된순</option>
         </select>
-        <Button text={"새 일기 쓰기"} type={"POSITIVE"} />
+        <Button onClick={()=> nav("/new")} text={"새 일기 쓰기"} type={"POSITIVE"} />
       </div>
       <div className="list_wrapper">
         {data.map((item)=><DiaryItem key={item.id} {...item}/>)}
